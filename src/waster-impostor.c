c860a620d223d5cc30ac2b191a090b74292dbebb
Timm Bäder <mail@baedert.org>
2018-12-23 08:09:46 +0100

    Fix impostor

--- src/waster-impostor.c
+++ src/waster-impostor.c
@@ -8,9 +8,14 @@ void
 ws_impostor_clone (WsImpostor *self,
                    GtkWidget  *widget)
 {
+  GdkPaintable *p;
+
   g_clear_object (&self->paintable);
 
-  self->paintable = gtk_widget_paintable_new (widget);
+  p = gtk_widget_paintable_new (widget);
+  self->paintable = gdk_paintable_get_current_image (p);
+
+  g_object_unref (p);
 }
 
 static void
@@ -18,22 +23,34 @@ ws_impostor_snapshot (GtkWidget   *widget,
                       GtkSnapshot *snapshot)
 {
   WsImpostor *self = (WsImpostor *)widget;
+  const int width = gtk_widget_get_width (widget);
+  const int height = gtk_widget_get_height (widget);
 
   if (self->paintable)
     {
       gdk_paintable_snapshot (self->paintable,
                               snapshot,
-                              gtk_widget_get_width (widget),
-                              gtk_widget_get_height (widget));
+                              width, height);
     }
+}
 
-  g_message ("%s, %p", __FUNCTION__, self->paintable);
+static void
+ws_impostor_finalize (GObject *object)
+{
+  WsImpostor *self = WS_IMPOSTOR (object);
+
+  g_clear_object (&self->paintable);
+
+  G_OBJECT_CLASS (ws_impostor_parent_class)->finalize (object);
 }
 
 static void
 ws_impostor_class_init (WsImpostorClass *class)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (class);
+
+  object_class->finalize = ws_impostor_finalize;
 
   widget_class->snapshot = ws_impostor_snapshot;
 }
